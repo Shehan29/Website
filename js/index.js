a72197248c39e5c94f891d9d59f9df530284fb82
shehan29 <shehan29@hotmail.com>
2020-07-28 23:10:27 -0400

    Remove google maps plugin

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 	// the body of this function is in assets/js/now-ui-kit.js
-	const coordinates = [43.478117, -80.537528];
-	const centre = [43.475378, -80.555895];
-	nowuiKit.initContactUsMap(coordinates, centre);
-	
+	// const coordinates = [43.478117, -80.537528];
+	// const centre = [43.475378, -80.555895];
+	// nowuiKit.initContactUsMap(coordinates, centre);
+
 	// hover animation for interesting facts
 	setupHoverAnimation('#yearsOfCoding', 'tada');
 	setupHoverAnimation('#possibilities', 'rubberBand');
 	setupHoverAnimation('#hackathons', 'pulse');
-	
+
 	// hover animation for work experience logos
 	setupHoverAnimation('#wishLogo', 'jello');
 	setupHoverAnimation('#ibmLogo', 'jello');
 	setupHoverAnimation('#capitalOneLogo', 'jello');
 	setupHoverAnimation('#niLogo', 'jello');
 	setupHoverAnimation('#brainweberLogo', 'jello');
-	
+
 	// hover animation for footer
 	setupHoverAnimation('#code', 'swing');
 });
